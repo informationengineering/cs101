381d3b364356b42bed71cf035d97bc6d61114e51
informationengineering <94364774+informationengineering@users.noreply.github.com>
2023-12-14 14:18:23 +0800

    Add files via upload

---
+++ EX2/main 1.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    if (i % 2 == 0){
+        printf("是");
+    }
+    else{
+        printf("否");
+    }
+    
+
+    return 0;
+}
